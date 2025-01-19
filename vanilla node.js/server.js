f722267943244ba0dfc3fb01c721c7c51479ee9f
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-01-19 16:09:18 +0200

    Add styles

--- vanilla node.js/server.js
+++ vanilla node.js/server.js
@@ -3,8 +3,15 @@ import http from "http";
 const PORT = 3000;
 
 import homePage from "./views/home/index.html.js";
+import styles from "./content/styles/site.css.js";
 
 http.createServer((req, res) => {
+    if (req.url.includes("/styles/site.css")) {
+        res.writeHead(200, { "content-type": "text/css" });
+        res.write(styles);
+        return res.end();
+    }
+
     res.writeHead(200, { "content-type": "text/html" });
     res.write(homePage);
     return res.end();
