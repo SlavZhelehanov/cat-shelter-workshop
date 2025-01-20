178499a39986dd3ac4d179b6b5e714a9aeeb09af
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-01-20 12:16:47 +0200

    Add global variabals cats and breeds for data from database

--- vanilla node.js/server.js
+++ vanilla node.js/server.js
@@ -6,6 +6,23 @@ const PORT = 3000;
 import homePage from "./views/home/index.html.js";
 import styles from "./content/styles/site.css.js";
 
+async function readJsonData(file) {
+    try {
+        return await fs.readFile(`./data/${file}.json`, "utf8", (err, data) => {
+            if (err) return console.error(err.message);
+            return data
+        });
+    } catch (readJsonData) {
+        console.log(`readJsonData: ${readJsonData}`);
+    }
+}
+
+let cats = await readJsonData("cats");
+cats = JSON.parse(cats);
+
+let breeds = await readJsonData("breeds");
+breeds = JSON.parse(breeds);
+
 http.createServer((req, res) => {
     function handleGetRequiest(contentType, file) {
         res.writeHead(200, { "content-type": contentType });
