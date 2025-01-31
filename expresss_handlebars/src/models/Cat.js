f60af583be38f0fced5f86bca62215503bde6dd9
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-01-31 19:04:20 +0200

    Add mongoose library and remove uuid

---
+++ expresss_handlebars/src/models/Cat.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+export default mongoose.model("Cat", new mongoose.Schema({
+    name: { type: String, required: [true, "Name field can't be empty"] },
+    description: { type: String, required: [true, "Description field can't be empty"] },
+    breed: { type: String, required: [true, "Breed field can't be empty"] },
+    price: { type: Number, min: [0, "Price must be larger than 0"] },
+    image: { type: String, required: [true, "Image field can't be empty"] }
+}, { timestamps: true }));
