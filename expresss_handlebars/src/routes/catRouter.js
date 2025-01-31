8fc20e87be2576ced62d658d21600424a6cf3ad1
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-01-31 19:50:39 +0200

    Render add breed page

--- expresss_handlebars/src/routes/catRouter.js
+++ expresss_handlebars/src/routes/catRouter.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import homeService from "../services/homeService.js";
+// import homeService from "../services/homeService.js";
+// import breedService from "../services/breedService.js";
 
 const catRouter = Router();
 
 // ADD CAT
 catRouter.get('/add-cat', async (req, res) => {
     return res.render('addCat');
+});
+catRouter.get('/add-breed', async (req, res) => {
+    return res.render('addBreed');
 });
 
 export default catRouter;
