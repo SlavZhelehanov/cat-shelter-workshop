81379d9e2b540ca820247794ea00a746e5c8a439
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-02-04 19:08:18 +0200

    Fix header partial

--- expresss_handlebars/src/routes/homeRouter.js
+++ expresss_handlebars/src/routes/homeRouter.js
@@ -3,9 +3,9 @@ import catService from "../services/catService.js";
 
 const homeRouter = Router();
 
-homeRouter.get('/', async (req, res) => { 
+homeRouter.get('/', async (req, res) => {
     const cats = await catService.getAllCats();
-    return res.render('home', { isHomePage: true, cats }); 
+    return res.render('home', { isHomePage: true, cats, isAuth: req.user });
 });
 
 export default homeRouter;
