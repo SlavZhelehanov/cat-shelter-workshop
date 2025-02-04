989ebee867eecda10cd53c5b8c4db6651bcf267e
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-02-04 19:09:51 +0200

    Fix 404 page navigation

--- expresss_handlebars/src/routes/index.js
+++ expresss_handlebars/src/routes/index.js
@@ -11,6 +11,6 @@ router.use("/cats", catRouter);
 
 router.use("/users", userRouter);
 
-router.all("*", (req, res) => { return res.render('404'); });
+router.all("*", (req, res) => { return res.render('404', { isAuth: req.user }); });
 
 export default router;
