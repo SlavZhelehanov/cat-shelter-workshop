95dd4a5056a2143be5f8fe747189f9a73d613751
SlavZhelehanov <slav.zhelehanov@gmail.com>
2025-02-05 17:14:04 +0200

    Fix details, change-info and new-home link

--- expresss_handlebars/src/routes/homeRouter.js
+++ expresss_handlebars/src/routes/homeRouter.js
@@ -5,7 +5,8 @@ const homeRouter = Router();
 
 homeRouter.get('/', async (req, res) => {
     let cats = await catService.getAllCats();
-    cats = cats.map(cat => cat = { ...cat._doc, isHomePage: true });
+    cats = cats.map(cat => cat = { ...cat._doc, isHomePage: true, id: cat._id });
+    
     return res.render('home', { isHomePage: true, cats, isAuth: req.user });
 });
 
